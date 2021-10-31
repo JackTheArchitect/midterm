022eb51c274d4834f46a4fee8e723e51e56320a8
Jaeuk Kim <soophan3@gmail.com>
2021-10-30 22:20:14 -0400

    done!

--- controllers/book.js
+++ controllers/book.js
@@ -49,38 +49,128 @@ module.exports.displayAddPage = (req, res, next) => {
     res.render('book/add_edit', {
         title: 'Add a new Item',
         book: book
-    })
-
-
-
-    // ADD YOUR CODE HERE        
+    })    
 
 }
 
 // Processes the data submitted from the Add form to create a new book
 module.exports.processAddPage = (req, res, next) => {
 
-    // ADD YOUR CODE HERE
+    let newBook = Book({
+        _id: req.body.id,
+        Title: req.body.Title,
+        Description: req.body.Description,
+        Price: req.body.Price,
+        Author: req.body.Author,
+        Genre: req.body.Genre
+    });
+    
+    Book.create(newBook, (err, Title) =>{ //? what is this title?
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            // refresh the book list
+            console.log(Title);
+            res.redirect('/book/list');
+        }
+    });
+
+    
+
+    
 
 }
 
 // Gets a book by id and renders the Edit form using the add_edit.ejs template
 module.exports.displayEditPage = (req, res, next) => {
+    let id = req.params.id;
+
+    Book.findById(id, (err, bookToShow) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);            
+        }
+        else
+        {
+            //show the edit view
+            res.render('book/add_edit', {
+                title: 'Edit Details', 
+                book: bookToShow
+            })
+        }
+    });
     
-    // ADD YOUR CODE HERE
 
 }
 
 // Processes the data submitted from the Edit form to update a book
 module.exports.processEditPage = (req, res, next) => {
     
-    // ADD YOUR CODE HERE
+    let id = req.params.id
+    
+    let editedBook = Book({
+        _id: req.body.id,
+        Title: req.body.Title,
+        Description: req.body.Description,
+        Price: req.body.Price,
+        Author: req.body.Author,
+        Genre: req.body.Genre
+    });
+    
+    Book.updateOne(editedBook, (err, Title) =>{ //? what is this title?
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            // refresh the book list
+            console.log(Title);
+            res.redirect('/book/list');
+        }
+    });
     
 }
 
 // Deletes a book based on its id.
 module.exports.performDelete = (req, res, next) => {
     
-    // ADD YOUR CODE HERE
+    // As the Mongoose document says remove() is deprecated 
+    // I am using findByIdAndDelete()
+    // reference: https://mongoosejs.com/docs/deprecations.html#remove
 
+    let id = req.params.id
+
+    // Book.remove(id, (err) => {
+    //     if(err)
+    //     {
+    //         console.log(err);
+    //         res.end(err);            
+    //     }
+    //     else
+    //     {
+    //         // refresh the book list
+    //         res.redirect('/book/list');
+    //     }
+    // });
+    
+    Book.findByIdAndDelete(id, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+            res.end(err);
+        }
+        else
+        {
+            // refresh the book list
+            res.redirect('/book/list');
+        }
+    });
 }
--- models/book.js
+++ models/book.js
@@ -3,7 +3,7 @@ let mongoose = require('mongoose');
 
 // Create a model class
 let bookModel = mongoose.Schema(
-    {
+    {        
         Title: String,
         Description: String,
         Price: Number,
